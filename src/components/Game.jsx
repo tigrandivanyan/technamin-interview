b9978294ddae64c74e1d2a70310c392694eb7976
Tigran Divanyan <tigrandivanyan>
2022-10-12 22:06:11 +0400

    made site beautiful (styles added)

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -2,23 +2,45 @@ import React from "react"
 
 function Game({game}) {
 
+  const {home, away, markets_count, match_info} = game ? game : {};
+
   return (
-    <div className="Sport">
+    <div className="game">
       <div className="text-value">
         <p>Home:</p>
-        <p>{game.home.name}</p>
+        {
+          home?.name ? 
+            <p>{home?.name}</p>
+          : 
+            <p style={{color:'red'}}>No home name</p>
+        }
       </div>
       <div className="text-value">
         <p>Away:</p>
-        <p>{game.away.name}</p>
+        {
+          away?.name ? 
+            <p>{away?.name}</p>
+          : 
+            <p style={{color:'red'}}>No away name</p>
+        }
       </div>
       <div className="text-value">
         <p>Markets Count:</p>
-        <p>{game.markets_count}</p>
+        {
+          markets_count ? 
+            <p>{markets_count}</p>
+          : 
+            <p style={{color:'red'}}>No markets count</p>
+        }
       </div>
       <div className="text-value">
         <p>Score:</p>
-        <p>{game.match_info.score}</p>
+        {
+          match_info?.score ? 
+            <p>{match_info?.score}</p>
+          : 
+            <p style={{color:'red'}}>No match info</p>
+        }
       </div>
     </div>
   );
